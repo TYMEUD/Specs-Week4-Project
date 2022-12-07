0f218eaffd3569a4f327ffe5b8305692ee8a7dab
TYMEUD <111309856+TYMEUD@users.noreply.github.com>
2022-12-07 07:37:52 -0700

    finishing up unit 4 project, need help with the SECRET env

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,6 +1,5 @@
-import {useState} from 'react'
 import axios from 'axios'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import AuthContext from '../store/authContext'
 
 
@@ -8,6 +7,8 @@ const Auth = () => {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     const [register, setRegister] = useState(true)
+    const [message, setMessage] = useState("")
+    const [display, setDisplay] = useState("none")
     
     const authCtx = useContext(AuthContext)
     
@@ -20,45 +21,55 @@ const Auth = () => {
             password
         }
         
-        const url = 'https://socialmtn.devmountain.com'
+        // const url = 'https://socialmtn.devmountain.com'
+        const url = "http://localhost:4545"
         
-        axios.post(register ? `${url}/register` : `${url}/login`, body)
-        .then(({data}) => {
-            console.log('AFTER AUTH', data)
-        })
-        .catch(err => {
-            setPassword('')
-            setUsername('')
-        })
-        
-        authCtx.login(res.data.token, res.data.exp, res.data.userId)
+        axios
+            .post(register ? `${url}/register` : `${url}/login`, body)
+            .then(res => {
+                console.log("AFTER AUTH", res.data)
+                authCtx.login(res.data.token, res.data.exp, res.data.userId)
+            })
+            .catch(err => {
+                setMessage(err.response.data)
+                setDisplay("block")
+                setPassword("")
+                setUsername("")
+            })
 }
 
     
 
 return (
     <main>
-           <h1>Welcome!</h1>
-           <form className='form auth-form' onSubmit={submitHandler}>
-               <input 
-                    type='text'
-                    placeholder='username'
-                    value={username}
-                    onChange={e => setUsername(e.target.value)}
-                    className='form-input'/>
-               <input
-                     type='password'
-                     placeholder='password'
-                     value={password}
-                     onChange={e => setPassword(e.target.value)}
-                     className='form-input'/>
-               <button className='form-btn'>
-                   {register ? 'Sign Up' : 'Login'}
-               </button>
-           </form>
-           <button className='form-btn'>Need to {register ? 'Login' : 'Sign Up'}?</button>
-       </main>
-   )
+        <h1>Welcome!</h1>
+        <form className="form auth-form" onSubmit={submitHandler}>
+            <input
+                type="text"
+                placeholder="username"
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+                className="form-input"
+            />
+            <input
+                type="password"
+                placeholder="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                className="form-input"
+            />
+            <button className="form-btn">
+                {register ? "Sign Up" : "Login"}
+            </button>
+        </form>
+        <p style={{ display: display }} className="auth-msg">
+            {message}
+        </p>
+        <button className="form-btn" onClick={() => setRegister(!register)}>
+            Need to {register ? "Login" : "Sign Up"}?
+        </button>
+    </main>
+)
 }
 
 export default Auth
--- src/components/Header.js
+++ src/components/Header.js
@@ -22,36 +22,36 @@ const Header = () => {
                 <h2>Social Mountain</h2>
             </div>
             <nav>
-            {
-                authCtx.token ? (
-                    <ul className='main-nav'>
-                        <li>
-                            <Link to='/'>Home</Link>
-                        </li>
-                        <li>
-                            <Link to='profile'>Profile</Link>
-                        </li>
-                        <li>
-                            <Link to='form'>Add Post</Link>
-                        </li>
-                        <li>
-                            <button className='logout-btn' onClick={() => authCtx.logout()}>Logout</button>
-                        </li>
-                    </ul>
-                ) : (
-                    <ul className='main-nav'>
-                        <li>
-                            <Link to='/'>Home</Link>
-                        </li>
-                        <li>
-                            <Link to='/auth'>Login or Sign Up</Link>
-                        </li>
-                    </ul>
-                )
-            }
-        </nav>
-    </header>
-)
+                {
+                    authCtx.token ? (
+                        <ul className='main-nav'>
+                            <li>
+                                <NavLink style={styleActiveLink} to='/'>Home</NavLink>
+                            </li>
+                            <li>
+                                <NavLink style={styleActiveLink} to='profile'>Profile</NavLink>
+                            </li>
+                            <li>
+                                <NavLink style={styleActiveLink} to='form'>Add Post</NavLink>
+                            </li>
+                            <li>
+                                <button className='logout-btn' onClick={() => authCtx.logout()}>Logout</button>
+                            </li>
+                        </ul>
+                    ) : (
+                        <ul className='main-nav'>
+                            <li>
+                                <NavLink style={styleActiveLink} to='/'>Home</NavLink>
+                            </li>
+                            <li>
+                                <NavLink style={styleActiveLink} to='/auth'>Login or Sign Up</NavLink>
+                            </li>
+                        </ul>
+                    )
+                }
+            </nav>
+        </header>
+    )
 }
 
 export default Header
